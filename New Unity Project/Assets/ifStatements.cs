3ab3eafa266cc9772271aad3479703d676205435
Alyssa Burdeos <lyssaburd93@gmail.com>
2017-02-09 11:34:17 -0700

    Created If Statements Scene and Script

---
+++ New Unity Project/Assets/ifStatements.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifStatements : MonoBehaviour {
+	private float Gouda = 7.0f;
+	private int Cheddar = 9;
+	private bool Open = true;
+	private string name = "Survey";
+
+	// Use this for initialization
+	void Start () {
+		if (Open) 
+		{
+			print ("Welcome to Cheese World! We only serve two types of cheese. Sorry to disappoint you.");
+		}
+	
+		if (!Open) 
+		{
+			print ("Welcome to Cheese World! We are currently closed. Sorry to disappoint you.");
+		}
+
+		if (name == "Survey") 
+		{
+			print ("What are your thoughts about cheese?");
+		}
+			
+		if (name != "Survey") 
+		{
+			print ("We don't care about your cheesy thoughts.");	
+		}
+
+		if (name == "PepperJack") 
+		{
+			print ("I am Pepper Jack, and I am the best cheese of all.");
+		}
+
+		if (Gouda == Cheddar) 
+		{
+			print ("Cheddar is just as Gouda.");
+		}
+		if (Gouda != Cheddar) 
+		{
+			print ("Gouda and Cheddar are different. And that is okay.");
+		}
+		if (Gouda < Cheddar) 
+		{
+			print ("Cheddar is better than Gouda.");
+		}
+		if (Gouda > Cheddar) 
+		{
+			print ("Gouda is better than Cheddar.");
+		}
+		if (Gouda <= Cheddar) 
+		{
+			print ("Gouda and Cheddar are both okay, I guess.");
+		}
+		if (Gouda >= Cheddar)
+		{
+			print ("I don't have a preference between Gouda and Cheddar.");
+		}
+
+	}
+}
