5151841af35e9aa58d55b8fb5c94e3b1697ff8c5
Alyssa Burdeos <lyssaburd93@gmail.com>
2017-02-28 11:27:18 -0700

    Lists Homework

--- New Unity Project/Assets/arraysHomework.cs
+++ New Unity Project/Assets/arraysHomework.cs
@@ -1,63 +1,63 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class arraysHomework : MonoBehaviour {
-
-	private int randomNum = 11;
-
-	string[] camp = { "band", "boot", "summer" };
-	/*string[] person1 = { "Alyssa", "Kyle", "Emerson", "Erin" };
-	string[] person2 = { "Sadie", "Travis", "Karma", "Gabe" };
-	string[] noun1 = { "wood", "rocks", "duct tape", "bricks", "mud" };
-	string[] structure = { "fire", "cabin", "bridge", "treehouse" };
-	string[] emotion = { "funny", "happy", "strange", "angry", "sad", "excited" };
-	string[] verb = { "sat", "cried", "laughed", "stared", "pondered" };
-	string[] pluralFood = { "cookies", "brownies", "berries", "sushi rolls", "steaks" };
-	string[] pastVerb = { "laughed", "cried", "sighed", "shrugged" };
-	int[] number = { 3,5,7,9,12 };
-	string[] pastVerb2 = { "burped", "coughed", "gagged", "grimaced" }; */
-
-
-
-
-		// Use this for initialization
-		void Start () {
-
-		// Example: adj1[Random.Range(0,adj1.Length)]
-
-		while(true)
-		{
-			camp [Random.Range (0, camp.Length)];
-			if (randomNum > 10) 
-			{
-				print ("This one time, at " + camp + "camp...");
-				/*person1[Random.Range (0, person1.Length)];
-				person2[Random.Range (0, person2.Length)];
-				noun1[Random.Range (0, noun1.Length)];
-				structure[Random.Range (0, structure.Length)];
-				emotion[Random.Range (0, emotion.Length)];
-				verb[Random.Range (0, verb.Length)];
-				pluralFood[Random.Range (0, pluralFood.Length)];
-				pastVerb[Random.Range (0, pastVerb.Length)];
-				number[Random.Range (0, number.Length)];
-				pastVerb2[Random.Range (0, pastVerb2.Length)];*/
-
-			} 
-			else if (randomNum < 10) 
-			{
-				print("Enter 11 to read a story");
-			}
-			if (randomNum == 10) 
-			{
-				break;
-			}	
-
-
-		}
-	}
-}
-
-/* Story: This one time, at [camp] camp, [person1] and [person2] were looking for [noun1] to build a [structure]. Along the way, 
-they began to feel very [emotion]. They [verb] for a while. Finally, [person2] said, "Hey, do you think there was something 
-in those [pluralFood] that we ate"?. [person1] [pastVerb] and said "I don't know! But I ate [number]!" The two [pastVerb2]
-and continued looking for [noun1]. The End. */
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class arraysHomework : MonoBehaviour {
+//
+//	private int randomNum = 11;
+//
+//	string[] camp = { "band", "boot", "summer" };
+//	/*string[] person1 = { "Alyssa", "Kyle", "Emerson", "Erin" };
+//	string[] person2 = { "Sadie", "Travis", "Karma", "Gabe" };
+//	string[] noun1 = { "wood", "rocks", "duct tape", "bricks", "mud" };
+//	string[] structure = { "fire", "cabin", "bridge", "treehouse" };
+//	string[] emotion = { "funny", "happy", "strange", "angry", "sad", "excited" };
+//	string[] verb = { "sat", "cried", "laughed", "stared", "pondered" };
+//	string[] pluralFood = { "cookies", "brownies", "berries", "sushi rolls", "steaks" };
+//	string[] pastVerb = { "laughed", "cried", "sighed", "shrugged" };
+//	int[] number = { 3,5,7,9,12 };
+//	string[] pastVerb2 = { "burped", "coughed", "gagged", "grimaced" };*/
+//
+//
+//
+//
+//		// Use this for initialization
+//		void Start () {
+//
+//		// Example: adj1[Random.Range(0,adj1.Length)]
+//
+//		while(true)
+//		{
+//			camp [Random.Range (0, camp.Length)];
+//			if (randomNum > 10) 
+//			{
+//				print ("This one time, at " + camp + "camp...");
+//				/*person1[Random.Range (0, person1.Length)];
+//				person2[Random.Range (0, person2.Length)];
+//				noun1[Random.Range (0, noun1.Length)];
+//				structure[Random.Range (0, structure.Length)];
+//				emotion[Random.Range (0, emotion.Length)];
+//				verb[Random.Range (0, verb.Length)];
+//				pluralFood[Random.Range (0, pluralFood.Length)];
+//				pastVerb[Random.Range (0, pastVerb.Length)];
+//				number[Random.Range (0, number.Length)];
+//				pastVerb2[Random.Range (0, pastVerb2.Length)];*/
+//
+//			} 
+//			else if (randomNum < 10) 
+//			{
+//				print("Enter 11 to read a story.");
+//			}
+//			if (randomNum == 11) 
+//			{
+//				break;
+//			}	
+//
+//
+//		}
+//	}
+//}
+//
+// /*Story: This one time, at [camp] camp, [person1] and [person2] were looking for [noun1] to build a [structure]. Along the way, 
+//they began to feel very [emotion]. They [verb] for a while. Finally, [person2] said, "Hey, do you think there was something 
+//in those [pluralFood] that we ate"?. [person1] [pastVerb] and said "I don't know! But I ate [number]!" The two [pastVerb2]
+//and continued looking for [noun1]. The End.*/
---
+++ New Unity Project/Assets/listsHomework.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class listsHomework : MonoBehaviour {
+	public string animal;
+	public List<string> zooRoster = new List<string>();
+
+
+	// Use this for initialization
+	void Start () {
+		zooRoster.Add ("monkey");
+		zooRoster.Add ("elephant");
+		zooRoster.Add ("alligator");
+	
+	}
+
+	public void AddAnimal()
+	{
+		if (animal != "") 
+		{
+			if (zooRoster.Contains (animal) != true)
+			{
+				zooRoster.Add (animal);
+			}
+		}
+	}
+
+	public void RemoveAnimal()
+	{
+		if (animal != "") 
+		{
+			if (zooRoster.Contains (animal) == true) 
+			{
+				zooRoster.Remove (animal);
+			}
+
+		}
+			
+	}
+	
+
+}
