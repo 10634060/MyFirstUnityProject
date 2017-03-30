6745922e262f3736c708df62fac5257c6124611b
Alyssa Burdeos <lyssaburd93@gmail.com>
2017-03-30 11:50:47 -0600

    MASS MERGE
    MASS MERGE

---
+++ Calculator/Assets/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class Calculator : MonoBehaviour {
+
+	public InputField input1;
+	public InputField input2;
+	public Text result;
+
+
+
+	void Start () {
+
+		AddNumbers ();
+	
+
+	}
+
+	public void AddNumbers()
+	{
+		int numInput;
+
+		numInput = int.Parse(input1.text) + int.Parse(input2.text);
+
+		result.text = numInput.ToString();
+		
+	}
+	
+
+
+}
---
+++ New Unity Project 2/Assets/Functions.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class Functions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void CurrentTime()
+	{
+		string greeting;
+		int hour = DateTime.Now.Hour;
+
+		if (hour < 12)
+			greeting = "Good morning";
+		else if (hour < 17)
+			greeting = "Good afternoon";
+		else
+			greeting = "Good evening";
+
+		print(string.Format ("{0}, the current time is {1}", greeting, DateTime.Now.ToLongTimeString()));
+		//Greeting = {0}, TimeOfDay = {1}. You can use these more than once.
+	}
+
+	public void CurrentDate()
+	{
+		string greeting = "Hello";
+
+		print (greeting + ", today is " + DateTime.Now.ToLongDateString() + ".");
+	}
+
+	public void DateAndTime()
+	{
+		CurrentTime ();
+		CurrentDate ();
+	}
+}
