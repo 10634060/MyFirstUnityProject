49b175b447d38642c9bab9cb70cc531401c4f642
Alyssa Burdeos <lyssaburd93@gmail.com>
2017-04-18 14:53:56 -0600

    Falling Number Script
    Created script for falling number action.

---
+++ Calculator/Assets/fallingNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+public class fallingNumber : MonoBehaviour {
+
+
+	public float fallSpeed = 5.0f;
+	public Text result;
+
+	public Text targetNum;
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+		while (result.text != targetNum.text) 
+		{
+			transform.Translate (Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+		}
+
+	}
+
+	/*public void Destroy()
+	{
+		if (result.text == targetNum.text) {
+			Destroy (targetNum);
+		} 
+		else 
+		{
+			print ("You Fail.");
+		}
+	}*/
+}
