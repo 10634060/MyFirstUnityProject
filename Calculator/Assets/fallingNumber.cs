bc3e97859b685587e9657100cb44e2aa6ad7c9f8
Alyssa Burdeos <lyssaburd93@gmail.com>
2017-04-20 12:59:10 -0600

    Button Deactivation

--- Calculator/Assets/fallingNumber.cs
+++ Calculator/Assets/fallingNumber.cs
@@ -5,16 +5,20 @@ using System.Collections;
 using System.Collections.Generic;
 public class fallingNumber : MonoBehaviour {
 
-
-	public float fallSpeed = 10.0f;
-	public Text result;
-
+	int randNum;
 	public Text targetNum;
+	public Vector3 spawn = new Vector3(0, 274, 0);
+
+
 
 	void Start () 
 	{
-		
+
+		GameObject targetSpawn = (GameObject)Instantiate(targetNum, new Vector3(0,2,0), transform.rotation);
+		randNum = UnityEngine.Random.Range (0, 101);
+		targetNum.text = randNum.ToString ();
 	}
+
 
 
 		
